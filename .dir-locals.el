1eb9938a71e8d4559c02804c574d743aca2b1b57
whatacold <whatacold@gmail.com>
2019-06-16 14:48:05 +0800

    Trivial test project for ppcompile.

---
+++ .dir-locals.el
@@ -0,0 +1,11 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil
+  (eval . (setq ppcompile-path-mapping-list
+                `(("/home/try/test/" . ,(file-name-directory (directory-file-name (file-name-directory (buffer-file-name))))))))
+  (ppcompile-remote-compile-command . "make -C /home/try/test/ppcompile-test-project")
+  (ppcompile-ssh-user . "try")
+  (ppcompile-ssh-host . "localhost")
+  (ppcompile-rsync-dst-dir . "/home/try/test/")))
+
